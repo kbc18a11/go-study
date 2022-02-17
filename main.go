1ce181fcab89acf2936a78a81c3e9aba75f657f0
kbc18a11 <wada121400@gmail.com>
2022-02-17 19:01:03 +0900

    次 https://qiita.com/k-penguin-sato/items/885a61d819cc431304f5

--- main.go
+++ main.go
@@ -2,10 +2,34 @@ package main
 
 import "fmt"
 
+type Person struct {
+	firstName string
+	age       int
+}
+
+func (a Person) name() string { //Personのメソッド
+	return a.firstName + " Personのやつ"
+}
+
+func (p Person) getAge() int {
+	return p.age
+}
+
+type User struct {
+	Person
+}
+
+func (a User) name() string { //Userのメソッド
+	return a.firstName + " Userのやつ"
+}
+
 func main() {
-	var var1 int = 10
-	var var2 *int = &var1
-	fmt.Println(var1)  //=> 10
-	fmt.Println(var2)  //=> 0x10414020
-	fmt.Println(*var2) //=> 10
+	bob := Person{"Bob", 10}
+	mike := User{}
+	mike.firstName = "Mike"
+	mike.age = 20
+
+	fmt.Println(bob.name())    //=> Bob
+	fmt.Println(mike.name())   //=> Mike
+	fmt.Println(mike.getAge()) //=> Mike
 }
