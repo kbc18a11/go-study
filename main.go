3b7f3df9d2aa86974dae25dd56328468796c2879
kbc18a11 <wada121400@gmail.com>
2022-02-19 21:56:00 +0900

    終わり

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Person struct{}  //Person構造体
 type Person2 struct{} //Person2構造体
@@ -24,9 +27,30 @@ func (p *Person2) intro() {
 }
 
 func main() {
-	bob := new(Person)
-	mike := new(Person2)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
 
-	IntroForPerson(bob)  //=> Hello World
-	IntroForPerson(mike) //=> Hello World
+	fmt.Println("Start!")
+
+	go func() {
+		process(2, "A")
+		ch1 <- true
+	}()
+
+	go func() {
+		process(2, "B")
+		ch2 <- true
+	}()
+
+	<-ch1
+	<-ch2
+
+	fmt.Println("Finish!")
+}
+
+func process(num int, str string) {
+	for i := 0; i <= num; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(i, str)
+	}
 }
